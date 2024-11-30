fe62d74b0785dda814bd27fa2ce77c737cff3a2e
thomlund <t.lundgaard@outlook.dk>
2024-11-30 10:12:11 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    pixel_array.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    pixel_array.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    pixel_array.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    pixel_array.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    pixel_array.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
+})
+input.onButtonPressed(Button.B, function () {
+    pixel_array.clear()
+    pixel_array.show()
+})
+let pixel_array: neopixel.Strip = null
+pixel_array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    basic.pause(100)
+    pixel_array.rotate(1)
+    pixel_array.show()
 })
